13880969c3fede497ad0cac270f1ffc18704f4ec
Tom Homewood <tomhomewood@gmail.com>
2020-06-06 14:28:13 +0100

    No longer putting files in /var due to lack of permissions when building on GitHub.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 # Install the platform tools
-/var/android-sdk/tools/bin/sdkmanager "platform-tools"
+/android/android-sdk/tools/bin/sdkmanager "platform-tools"
 # Install the build-tools
-/var/android-sdk/tools/bin/sdkmanager "build-tools;29.0.2"
+/android/android-sdk/tools/bin/sdkmanager "build-tools;29.0.2"
 # Install the Android and Google m2 repositories
-/var/android-sdk/tools/bin/sdkmanager "extras;android;m2repository"
-/var/android-sdk/tools/bin/sdkmanager "extras;google;m2repository"
+/android/android-sdk/tools/bin/sdkmanager "extras;android;m2repository"
+/android/android-sdk/tools/bin/sdkmanager "extras;google;m2repository"
 # Install Google Play Services
-/var/android-sdk/tools/bin/sdkmanager "extras;google;google_play_services"
+/android/android-sdk/tools/bin/sdkmanager "extras;google;google_play_services"
 # Install the specified platforms
-/var/android-sdk/tools/bin/sdkmanager $ANDROID_PLATFORMS
+/android/android-sdk/tools/bin/sdkmanager $ANDROID_PLATFORMS
 
 # Now call the original start in the parent VSTS image
 /bin/bash /vsts/start.sh
--- install-android-sdk.sh
+++ install-android-sdk.sh
@@ -1,8 +1,8 @@
 # Download and extract the sdk tools
-mkdir /var/tmp/android-sdk
-wget "https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip" -O /var/tmp/android-sdk/android-sdk.zip
+mkdir /android/tmp/android-sdk
+wget "https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip" -O /android/tmp/android-sdk/android-sdk.zip
 #wget --no-check-certificate "https://temp.tomhomewood.co.uk/sdk-tools-linux-4333796.zip" -O /var/tmp/android-sdk/android-sdk.zip
-unzip /var/tmp/android-sdk/android-sdk -d /var/android-sdk
-yes | /var/android-sdk/tools/bin/sdkmanager --licenses
-rm -rf /var/tmp/android-sdk
+unzip /android/tmp/android-sdk/android-sdk -d /android/android-sdk
+yes | /android/android-sdk/tools/bin/sdkmanager --licenses
+rm -rf /android/tmp/android-sdk
 # SDK tools are now installed :)
