1af31ed0e38ef417e9320932dd0995e2c1267c16
Tom Homewood <tomhomewood@gmail.com>
2020-06-06 14:39:20 +0100

    Added a dash of sudo.

--- install-android-sdk.sh
+++ install-android-sdk.sh
@@ -1,8 +1,8 @@
 # Download and extract the sdk tools
-mkdir /android/tmp/android-sdk
-wget "https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip" -O /android/tmp/android-sdk/android-sdk.zip
+sudo mkdir /android/tmp/android-sdk
+sudo wget "https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip" -O /android/tmp/android-sdk/android-sdk.zip
 #wget --no-check-certificate "https://temp.tomhomewood.co.uk/sdk-tools-linux-4333796.zip" -O /var/tmp/android-sdk/android-sdk.zip
-unzip /android/tmp/android-sdk/android-sdk -d /android/android-sdk
-yes | /android/android-sdk/tools/bin/sdkmanager --licenses
-rm -rf /android/tmp/android-sdk
+sudo unzip /android/tmp/android-sdk/android-sdk -d /android/android-sdk
+sudo yes | /android/android-sdk/tools/bin/sdkmanager --licenses
+sudo rm -rf /android/tmp/android-sdk
 # SDK tools are now installed :)
